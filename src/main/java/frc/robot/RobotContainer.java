0228d5d0bb71160d555917f7fe9fc71c3becee56
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-07-18 21:44:05 +0800

    Add subsystems for Swerve, Intake, and Climber; implement mode-specific initialization in RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,47 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystem.climber.Climber;
+import frc.robot.subsystem.intake.Intake;
+import frc.robot.subsystem.swerve.Swerve;
+import lombok.Getter;
+import lombok.Setter;
 
 public class RobotContainer {
+  // subsystem
+  private final Swerve swerve;
+  private final Intake intake;
+  private final Climber climber;
+
+  // states
+  boolean g_isClimbing = false;
+  boolean s_intakeHasCoral = false;
+  boolean s_armHasCoral = false;
+  boolean s_armHasAlgae = false;
+
   public RobotContainer() {
+    if(Constants.MODE.equals(Constants.Mode.REAL)) {
+      swerve =Swerve.createReal();
+      intake = Intake.createReal();
+      climber = Climber.createReal();
+    } else if (Constants.MODE.equals(Constants.Mode.SIM)) {
+      swerve = Swerve.createSim();
+      intake = Intake.createSim(()->s_intakeHasCoral);
+      climber = Climber.createSim();
+    }else{
+      swerve = Swerve.createIO();
+      intake = Intake.createIO();
+      climber = Climber.createIO();
+    }
+
     configureBindings();
   }
 
   private void configureBindings() {}
+
+  private void configureSimulation(){}
+
+  private void configureVisualization(){}
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
