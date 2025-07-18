5aaa2e77e73a45076315960ce236bce5a35ddea0
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-07-18 14:15:35 +0800

    Add GamePiece and AllianceFlipUtil classes for game piece management and coordinate flipping

---
+++ src/main/java/frc/reefscape/GamePiece.java
@@ -0,0 +1,30 @@
+package frc.reefscape;
+
+import edu.wpi.first.math.util.Units;
+import lombok.Getter;
+
+public class GamePiece {
+  public static class Coral {
+    public static final double LENGTH = Units.inchesToMeters(11.0 + 7.0 / 8.0);
+    public static final double DIAMETER = Units.inchesToMeters(4.0);
+    public static final int OBJ_DETECT_ID = 1;
+  }
+
+  public static class Algae {
+    public static final double DIAMETER = Units.inchesToMeters(16.0);
+    public static final int OBJ_DETECT_ID = 0;
+  }
+
+  @Getter
+  public enum GamePieceType {
+    INVALID("Invalid"),
+    ALGAE("Algae"),
+    CORAL("Coral");
+
+    private final String name;
+
+    GamePieceType(String name) {
+      this.name = name;
+    }
+  }
+}
---
+++ src/main/java/frc/robot/RobotGoal.java
@@ -0,0 +1,47 @@
+package frc.robot;
+
+import frc.reefscape.GamePiece.GamePieceType;
+import lombok.Getter;
+import lombok.Setter;
+
+public class RobotGoal {
+  @Getter private final GamePieceType selectedType;
+  @Getter private final String selectedBranch;
+  @Getter private final String selectedLevel;
+  @Getter @Setter private Boolean ignoreArmMoveCondition;
+
+  public RobotGoal(
+      GamePieceType type, String branch, String level, Boolean ignoreArmMoveCondition) {
+    this.selectedType = type;
+    this.selectedBranch = branch;
+    this.selectedLevel = level;
+    this.ignoreArmMoveCondition = ignoreArmMoveCondition;
+  }
+
+  public static RobotGoal scoreNet() {
+    return new RobotGoal(GamePieceType.ALGAE, "N", "0", false);
+  }
+
+  public static RobotGoal scoreProcessor() {
+    return new RobotGoal(GamePieceType.ALGAE, "P", "0", false);
+  }
+
+  public static RobotGoal collectAlgae(String branch) {
+    return new RobotGoal(GamePieceType.ALGAE, branch, "0", false);
+  }
+
+  public static RobotGoal scoreCoral(String branch, String level) {
+    return new RobotGoal(GamePieceType.CORAL, branch, level, false);
+  }
+
+  public static RobotGoal invalid() {
+    return new RobotGoal(GamePieceType.INVALID, "NA", "NA", false);
+  }
+
+  public boolean isValid() {
+    return !selectedType.equals(GamePieceType.INVALID)
+        && !selectedBranch.equals("NA")
+        && !selectedLevel.equals("NA");
+  }
+  
+}
